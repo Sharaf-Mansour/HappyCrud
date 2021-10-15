9503c5cd6f01fc9f8921862f16fcf84e70164f69
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2021-10-15 11:26:54 +0200

    Fix First item editing

--- CRUDExtantions.cs
+++ CRUDExtantions.cs
@@ -1,27 +1,31 @@
 ﻿using static HappyCRUD.CRUD;
 namespace HappyCRUD;
-public  static class CRUDExtantions
+public static class CRUDExtantions
 {
-        private static int PrevIndex { get; set; } = -1;
-        private static object? TItem { get; set; } = new();
-       private static void Swap<T>(this IList<T>? Items, int Index, int dir)
-       {
+    private static int PrevIndex { get; set; } = -1;
+    private static object? TItem { get; set; } = new();
+    private static bool IsStartItem {  get; set; } = false;
+    private static void Swap<T>(this IList<T>? Items, int Index, int dir)
+    {
         T Current = Items![Index];
         Items![Index] = Items![Index + dir];
         Items![Index + dir] = Current;
-       }
+    }
     /// <summary>
     /// A Method checks if all items in the list are valid 
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="Items"></param>
     /// <returns>True if valid or has no elements and False if not valid</returns>
-    public static bool IsModelValid<T>(this IList<T> Items) where T : IValidation => Items.All(_ => _.IsValid());
-        public static void Create<T>(this IList<T>? Items) where T : IValidation, new()
+    public static bool IsModelValid<T>(this IList<T> Items) where T : IValidation => Items.All(_ => _.IsValid()) && Items.All(_ => !_.InEditState);
+    public static void Create<T>(this IList<T>? Items) where T : IValidation, ICloneable, new()
+    {
+        if (!IsInEditState  && Items!.IsModelValid())
         {
-            if (!IsInEditState  && Items!.IsModelValid())
-                Items!.Add(new());
+            Items!.Add(new() { InEditState = true});
+            Items!.StartEdit(Items.Count -1);
         }
+    }
     /// <summary>
     /// Begin edit state and mark item for edit: Needs Index
     /// </summary>
@@ -30,10 +34,14 @@ public  static class CRUDExtantions
     /// <param name="Index">Pass the item's index to edit</param>
     public static void StartEdit<T>(this IList<T>? Items, int Index) where T : ICloneable, IValidation
     {
-        if (!IsInEditState)
+        if (!IsInEditState &&  Items!.IsModelValid() && !Items![Index].InEditState)
         {
             TItem = (T)Items![Index].Clone();
             (Items![Index].InEditState, IsInEditState, PrevIndex) = (true, true, Index);
+        }
+        if (!IsInEditState &&  Items![Index].InEditState)
+        {
+            (IsStartItem,Items![Index].InEditState, IsInEditState, PrevIndex) = (true, true, true, Index);
         }
     }
     /// <summary>
@@ -41,37 +49,53 @@ public  static class CRUDExtantions
     /// </summary>  
     /// <typeparam name="T"></typeparam>
     /// <param name="Items"></param>
-    public static void Cancel<T>(this IList<T>? Items)
+    public static void Cancel<T>(this IList<T>? Items) 
+    {
+        if (IsInEditState && !IsStartItem)
         {
-            if (IsInEditState)
-            {
-                Items![PrevIndex] = (T)TItem!;
-                (PrevIndex, IsInEditState, TItem) = (-1, false, new());
-            }
+            Items![PrevIndex] = (T)TItem!;
+            (PrevIndex, IsInEditState, TItem) = (-1, false, new());
         }
+        else if (IsInEditState && IsStartItem)
+        {
+            Items?.RemoveAt(Items!.Count -1);
+            (IsStartItem, PrevIndex, IsInEditState, TItem) = (false, -1, false, new());
+        }
+    }
     /// <summary>
     /// Exit you from edit state and undo unsaved changes
     /// </summary>  
     /// <typeparam name="T"></typeparam>
     /// <param name="Items"></param>
-    public static void Delete<T>(this IList<T>? Items) where T : IValidation
+    public static void Delete<T>(this IList<T>? Items) 
+    {
+        if (IsInEditState )
         {
-            if (IsInEditState && !Items![PrevIndex].IsValid())
-            {
-                Items?.RemoveAt(PrevIndex);
-                (PrevIndex, IsInEditState, TItem) = (-1, false, new());
-            }
+            Items?.RemoveAt(PrevIndex);
+            (PrevIndex, IsInEditState, TItem) = (-1, false, new());
         }
+        else if (!IsInEditState && IsStartItem)
+        {
+            Items?.RemoveAt(Items!.Count -1);
+            (IsStartItem,PrevIndex, IsInEditState, TItem) = (false, -1, false, new());
+        }
+    }
     /// <summary>
     /// Save changes if item is valid
     /// </summary>  
     /// <typeparam name="T"></typeparam>
     /// <param name="Items"></param>
     public static void Save<T>(this IList<T>? Items) where T : IValidation
+    {
+        if (IsInEditState && Items![PrevIndex].IsValid() && !IsStartItem)
         {
-            if (IsInEditState && Items![PrevIndex].IsValid()) { 
-                Items![PrevIndex].InEditState = false;
+            Items![PrevIndex].InEditState = false;
             (PrevIndex, IsInEditState, TItem) = (-1, false, new());
+        }
+        else if (IsInEditState && IsStartItem && Items![Items!.Count -1].IsValid()  )
+        {
+            Items![Items!.Count -1].InEditState = false;
+            (IsStartItem,PrevIndex, IsInEditState, TItem) = (false, -1, false, new());
         }
     }
     /// <summary>
@@ -81,9 +105,9 @@ public  static class CRUDExtantions
     /// <param name="Items"></param>
     /// <param name="Index">Pass current item's index</param>
     public static void MoveUp<T>(this IList<T>? Items, int Index) where T : IValidation
-        {
-            if (Index > 0 && Items!.IsModelValid()) Items.Swap(Index, -1);
-        }
+    {
+        if (Index > 0 && Items!.IsModelValid()) Items.Swap(Index, -1);
+    }
     /// <summary>
     /// Swap with next item: Needs Index
     /// </summary>
@@ -91,8 +115,9 @@ public  static class CRUDExtantions
     /// <param name="Items"></param>
     /// <param name="Index">Pass current item's index</param>
     public static void MoveDown<T>(this IList<T>? Items, int Index) where T : IValidation
-        {
-            if (Index < Items!.Count - 1 && Items!.IsModelValid()) Items.Swap(Index, +1);
-        }
-   // public static void Set<T>(this IList<T>? items, T item) => items![PrevIndex] = item;
+    {
+        if (Index < Items!.Count - 1 && Items!.IsModelValid()) Items.Swap(Index, +1);
+    }
+    // public static void Set<T>(this IList<T>? items, T item) => items![PrevIndex] = item;
+    //public static IList<T>? SetAsIndex<T>(this IList<T>? Items, int Index) { PrevIndex = Index; return Items; }
 }
